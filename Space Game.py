68e297cd43613967416587b9dd31f489f2cccf1c
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2022-02-22 15:38:14 -0500

    Starting

---
+++ Space Game.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+import random
+import time
+import sys
+
+
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((1500, 800))
+
+
+start_game = True
+
+while start_game == True:
+    for window in pygame.event.get():
+        if window.type ==pygame.QUIT:
+            start_game = False
+        
