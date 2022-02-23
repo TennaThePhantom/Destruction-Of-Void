f9633f119ed2a514634cd51e71c8c34d97fe76c4
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2022-02-23 15:04:45 -0500

    images changes

--- Space Game.py
+++ Space Game.py
@@ -3,14 +3,32 @@ from pygame.locals import *
 import random
 import time
 import sys
+import os
 
 
+WIDTH = 1000
+HEIGHT = 800
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+TITLE = pygame.display.set_caption("Destruction-Of-Void")
+ICON = pygame.display.set_icon(pygame.image.load(os.path.join("Images", "void.jpg")))
 
 
+ENEMY_SPACE_SHIP1 = pygame.image.load(os.path.join("Images", "enemy_ship_1.png" ))
+ENEMY_SPACE_SHIP2 = pygame.image.load(os.path.join("Images", "enemy_ship_2.png" ))
+ENEMY_SPACE_SHIP3 = pygame.image.load(os.path.join("Images", "enemy_ship_3.png" ))
 
-pygame.init()
+# player ship
+USER_SPACE_SHIP = pygame.image.load(os.path.join("Images", "space_ship.png" ))
 
-screen = pygame.display.set_mode((1000, 800))
+
+RED_lASER = pygame.image.load(os.path.join("Images", "laser_red.png" ))
+GREEN_LASER = pygame.image.load(os.path.join("Images", "laser_green.png" ))
+BLUE_LASER = pygame.image.load(os.path.join("Images", "laser_blue.png" ))
+YELLOW_LASER = pygame.image.load(os.path.join("Images", "laser_yellow.png" ))
+
+
+BG = pygame.image.load(os.path.join("Images", "space_background.jpg" ))
+
 
 
 start_game = True
