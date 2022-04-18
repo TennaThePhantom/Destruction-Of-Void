72cfffb6dee7bce9d578aae3349adeadb09d8b37
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2022-04-18 15:55:20 -0400

    MegaEnemies

--- Mega_enemies.py
+++ Mega_enemies.py
@@ -127,7 +127,6 @@ class MegaEnemies1:
 
 
 
-
 class MegaEnemies2:
     COOLDOWN = 30
 
@@ -174,6 +173,8 @@ class MegaEnemies2:
             self.laser_countdown = 0
         elif self.laser_countdown > 0:
             self.laser_countdown += 1
+
+
 
 class MegaEnemies3:
     COOLDOWN = 3
@@ -279,15 +280,17 @@ class MegaEnemies4:
         elif self.laser_countdown > 0:
             self.laser_countdown += 1
 
+
+
 class Mega1(MegaEnemies1):
 
-    COLOR_MAP = {
+    ENEMY_MAP = {
             "Mega_Enemy": (MEGA_ENEMY1, PURPLE_FLAME_ENERGY_BALL),
             }
     def __init__(self, x, y, color, health=100, damage=160):
         super().__init__(x, y, health, damage)
-        self.ship_img, self.laser_img = self.COLOR_MAP[color]
-        self. mask = pygame.mask.from_surface(self.ship_img)
+        self.ship_img, self.laser_img = self.ENEMY_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
     
     def move(self, movemoment):
         self.y += movemoment
@@ -299,15 +302,16 @@ class Mega1(MegaEnemies1):
             self.laser_countdown = 1
 
 
+
 class Mega2(MegaEnemies2):
 
-    COLOR_MAP = {
+    ENEMY_MAP = {
             "Mega_Enemy": (MEGA_ENEMY2, GOLD_ENERGY_BALL),
             }
     def __init__(self, x, y, color, health=100, damage=200):
         super().__init__(x, y, health, damage)
-        self.ship_img, self.laser_img = self.COLOR_MAP[color]
-        self. mask = pygame.mask.from_surface(self.ship_img)
+        self.ship_img, self.laser_img = self.ENEMY_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
     
     def move(self, movemoment):
         self.y += movemoment
@@ -319,15 +323,16 @@ class Mega2(MegaEnemies2):
             self.laser_countdown = 1
 
 
+
 class Mega3(MegaEnemies3):
 
-    COLOR_MAP = {
+    ENEMY_MAP = {
             "Mega_Enemy": (MEGA_ENEMY3, SPARKING_ENERGY_BALL, SPARKING_ENERGY_BALL, SPARKING_ENERGY_BALL),
             }
     def __init__(self, x, y, color, health=100, damage=150):
         super().__init__(x, y, health, damage)
-        self.ship_img, self.laser_img, self.laser_img2, self.laser_img3 = self.COLOR_MAP[color]
-        self. mask = pygame.mask.from_surface(self.ship_img)
+        self.ship_img, self.laser_img, self.laser_img2, self.laser_img3 = self.ENEMY_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
     
     def move(self, movemoment):
         self.y += movemoment
@@ -343,15 +348,16 @@ class Mega3(MegaEnemies3):
             self.laser_countdown = 1
 
 
+
 class Mega4(MegaEnemies4):
 
-    COLOR_MAP = {
+    ENEMY_MAP = {
             "Mega_Enemy": (MEGA_ENEMY4, DARKBLUE_LIGHT_LASER, DARKBLUE_LIGHT_LASER),
             }
     def __init__(self, x, y, color, health=100, damage=180):
         super().__init__(x, y, health, damage)
-        self.ship_img, self.laser_img, self.laser_img2 = self.COLOR_MAP[color]
-        self. mask = pygame.mask.from_surface(self.ship_img)
+        self.ship_img, self.laser_img, self.laser_img2 = self.ENEMY_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
     
     def move(self, movemoment):
         self.y += movemoment
@@ -363,3 +369,8 @@ class Mega4(MegaEnemies4):
             laser2 = Laser(self.x + 20, self.y + 150, self.laser_img2)
             self.lasers.append(laser2)
             self.laser_countdown = 1
+
+
+
+
+
