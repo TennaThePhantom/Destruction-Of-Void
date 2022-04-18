3bc7b9cfc6806ef29985302657168d593242428c
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2022-04-18 15:49:48 -0400

    Lasers & Energy Balls

--- Lasers_EnergyBalls.py
+++ Lasers_EnergyBalls.py
@@ -3,13 +3,14 @@ import os
 from pygame.locals import *
 from SpaceGameVariables import *
 
+
 RED_lASER = pygame.image.load(os.path.join("Images", "Guns", "laser_red.png" ))
 GREEN_LASER = pygame.image.load(os.path.join("Images", "Guns", "laser_green.png" ))
 BLUE_LASER = pygame.image.load(os.path.join("Images", "Guns", "laser_blue.png" ))
 YELLOW_LASER = pygame.image.load(os.path.join("Images", "Guns", "laser_yellow.png" ))
 RED_ENERGY_BLAST = pygame.image.load(os.path.join("Images", "Guns", "Red_energy_ball.png" ))
 BLUE_AND_DARKBLUE_lASER = pygame.image.load(os.path.join("Images", "Guns", "Blue_DarkBlue_Laser.png" ))
-GOLDEN_LASER_MISSILE = pygame.image.load(os.path.join("Images", "Guns", "Golden_laser_missle.png" ))
+GOLDEN_LASER_MISSILE = pygame.image.load(os.path.join("Images", "Guns", "Golden_laser_missile.png" ))
 PURPLE_ENERGY_BALL = pygame.image.load(os.path.join("Images", "Guns", "Purple_energy_ball.png" ))
 ENERGY_STAR_BALL = pygame.image.load(os.path.join("Images", "Guns", "Star_ball.png" ))
 SPARKING_ENERGY_BALL = pygame.image.load(os.path.join("Images", "Guns", "Sparking_energy_ball.png" ))
@@ -18,3 +19,6 @@ GOLD_ENERGY_BALL = pygame.image.load(os.path.join("Images", "Guns", "Gold_energy
 BLUE_ENERGY_LASER = pygame.image.load(os.path.join("Images", "Guns", "Blue_energy_laser.png" ))
 PURPLE_FLAME_ENERGY_BALL = pygame.image.load(os.path.join("Images", "Guns", "Purple_Flame_Energy_Ball.png" ))
 BLUE_ENERGY_BALL = pygame.image.load(os.path.join("Images", "Guns", "Blue_energy_ball.png" ))
+
+
+
